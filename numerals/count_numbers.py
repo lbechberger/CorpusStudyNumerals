3c6183124cdbdbad9b8b7e5e6e1753f0988ebfee
Lucas Bechberger <lbechberger@uos.de>
2017-06-08 17:10:16 +0200

    fixed regex, improved speed significantly

--- numerals/count_numbers.py
+++ numerals/count_numbers.py
@@ -13,18 +13,20 @@ in_filename = sys.argv[1]   # input file to read (i.e., corpus)
 n = sys.argv[2]             # look at all numbers between 0 and n-1
 
 counts = [0] * int(n)
-regexes = []
-for i in range(int(n)):     # pre-compile regexes to save some execution time
-    regexes.append(re.compile('[ -\$"\[\(]'+str(i) +'[ \.,\!\?:-€;\]\)"]'))
+# regex to search for numbers --> precompile for efficiency
+regex = re.compile('^.*\t.*[ -\$"\[\(](\d+)[ \.,\!\?:-€;\]\)"].*$')
 
+num_lines = 0
 with open(in_filename, 'r') as f:
     for line in f:
-        match = re.search('^.*%(.*)$', line)  
-        if (match != None):
-            sentence = match.group(1)
-            for i in range(int(n)):
-                match_numeral = regexes[i].search(sentence)
-                if match_numeral != None:
-                    counts[i] += 1
+        match = regex.search(line)
+        if match != None:
+            number = int(match.group(1))        # found a number: add it to list if necessary
+            if number >= 0 and number < int(n):
+                counts[number] += 1
+        num_lines += 1
+        if num_lines % 500000 == 0: # progress bar
+            print num_lines
 
 print counts
+print num_lines
