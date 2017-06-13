7246f0326f5aed40e885083874ce540ded28e00a
Lucas Bechberger <lbechberger@uos.de>
2017-06-13 13:27:32 +0200

    added bar plot

--- numerals/count_numbers.py
+++ numerals/count_numbers.py
@@ -8,25 +8,30 @@ Simply count the number of occurrence for different numbers.
 """
 
 import sys, re
+import matplotlib.pyplot as plt
 
 in_filename = sys.argv[1]   # input file to read (i.e., corpus)
-n = sys.argv[2]             # look at all numbers between 0 and n-1
+n = int(sys.argv[2])             # look at all numbers between 0 and n-1
 
-counts = [0] * int(n)
+counts = [0] * n
 # regex to search for numbers --> precompile for efficiency
 regex = re.compile('^.*\t.*[ -\$"\[\(](\d+)[ \.,\!\?:-€;\]\)"].*$')
 
+print "starting to process..."
 num_lines = 0
 with open(in_filename, 'r') as f:
     for line in f:
         match = regex.search(line)
         if match != None:
             number = int(match.group(1))        # found a number: add it to list if necessary
-            if number >= 0 and number < int(n):
+            if number >= 0 and number < n:
                 counts[number] += 1
         num_lines += 1
         if num_lines % 500000 == 0: # progress bar
-            print num_lines
+            print "...{0} lines".format(num_lines)
 
-print counts
-print num_lines
+print "processed {0} lines".format(num_lines)
+
+# plot a bar chart
+plt.bar(range(n), counts)
+plt.show()
