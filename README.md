7131e02a104343ab9ea2ba39d461d738c5ca0bdb
Ulf Krumnack <krumnack@uos.de>
2017-07-21 17:29:01 +0200

    refactoring of the original script

--- README.md
+++ README.md
@@ -1,13 +1,99 @@
 # CorpusStudyNumerals
 Some scripts for a corpus study on numerals
 
-## count_numbers.py
 
-count_numbers.py can be used as follows:
+## Installation
+
+To package is more or less self contained. There are no hard
+requirements to run the programs. However, to actually use it in a
+reasonable way, you need the following:
+* [MatPlotLib](https://matplotlib.org/)  (if you want to create plots)
+* some corpus, i.e. some plain text file containing example sentences.
+  you may use data from the Leipzig [Wortschatz](http://wortschatz.uni-leipzig.de)
+  project
+
+
+## The program `count_numbers.py`
+
+The program `count_numbers.py` (in the subdirectory `numerals/`)is the
+main program of the package.  It can be used as follows:
+
 ```python
-python count_numbers.py path/to/corpus/file.txt 100 200 en
+python count_numbers.py --plot path/to/corpus/file.txt
 ```
 
-This script plots the number of occurrences for each number from 100 to 200 (including both borders), assuming English separators (i.e., "1,000" = "1000"). The last parameter can also be set to "de" (which results in using points to separate digits: "1.000" = "1000").
-For the plots under /plots, the file eng_news_2015_3M-sentences.txt from the Leipzig corpus was used.
+The script then plots the number of occurrences for each number from
+1 to 100 (including both borders). To change the range of interest,
+you can specify the boundaries on the command line:
 
+```python
+python count_numbers.py --plot --min=10 --max=1000 path/to/corpus/file.txt
+```
+
+The script usually assumes English texts and behaves accordingly
+(e.g., using "," as a thousands separator "1,000" = "1000"). The
+language can also be changed using a command line option:
+
+```python
+python count_numbers.py --plot --language=de path/to/corpus/file.txt
+```
+
+### Processing Wortschatz data
+
+If you work on the Leipzig
+[Wortschatz](http://wortschatz.uni-leipzig.de) data, you may set the
+environemnt variable `WORTSCHATZ_ROOT` to point to the directory where
+the Wortschatz data have been unpacked. With this setting it is
+sufficent to provide only the name of the corpus on the command line,
+i.e.
+
+```python
+export WORTSCHATZ_ROOT=/path/to/the/wortschatz/data
+python count_numbers.py --plot eng_news_2015_1M
+```
+
+should behave identical to
+
+```python
+python count_numbers.py --plot /path/to/the/wortschatz/data/eng_news_2015_1M/eng_news_2015_1M-sentences.txt
+```
+
+### The Processor
+
+The `Processor` (implemented in `processor.py`) does the actual
+work. It provides methods for searching numerals in an input stream
+and for plotting the results.
+
+
+### The Languages
+
+The `Language`s implemented in `languages.py` provide language
+specific information to be used during processing.
+
+
+## Plots
+
+For the plots under `plots/`, the file `eng_news_2015_3M-sentences.txt`
+from the Leipzig corpus was used.
+
+
+## Tests
+
+There are some (necessarily) incomplete unit tests in the `tests/`
+directory (see the [Python documentation on unit
+testing](https://docs.python.org/3/library/unittest.html) to learn how
+to add more tests).  You may run the tests by typing (being in the
+project root directory):
+
+```python
+python -m unittest tests.test_languages
+```
+
+The file `tests/test_regex.py` does not actually test parts of this
+package but is intended for experimenting with regular expressions.
+
+
+## Compatibility
+
+We try to make the code compatible with (recent vesions of) Python 2
+and Python 3. If you experience any problems, please let as know!
--- numerals/count_numbers.py
+++ numerals/count_numbers.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
 """
 Created on Thu Jun  8 16:16:12 2017
 
@@ -7,46 +8,83 @@ Simply count the number of occurrence for different numbers.
 @author: lbechberger
 """
 
-import sys, re
-import matplotlib.pyplot as plt
+from __future__ import print_function
 
-in_filename = sys.argv[1]   # input file to read (i.e., corpus)
-n_low = int(sys.argv[2])    # look at all numbers >= n_low
-n_high = int(sys.argv[3])   # and <= n_high
+import os
+import sys
+import locale
+import argparse
 
-# language of the corpus determines number separators (1,000 vs. 1.000), assume English if not given
-language = sys.argv[4] if len(sys.argv) >= 5 else "en"    
-separator = ","
-if language == "de":
-    separator = "."
+from languages import Language
+from processor import Processor
 
-counts = {}
-# regex to search for numbers --> precompile for efficiency
-regex = re.compile('[ \-\$"\[\(](\d+(\{0}\d\d\d)*)[ \.,\!\?:\-€;\]\)"]'.format(separator))
+locale.setlocale(locale.LC_ALL, '')
 
-print("starting to process...")
-num_lines = 0
-with open(in_filename, 'r') as f:
-    for line in f:
-        sentence = line.split("\t")[1]      # manually remove everything before the tabulator
-        matches = regex.findall(sentence)   # look for ALL occurences of numbers
-        for match in matches:
-            text = match[0].replace(separator, "")
-            number = int(text)
-            if number >= n_low and number <= n_high:
-                if not number in counts:    # not yet in dictionary: new entry
-                    counts[number] = 1
-                else:                       # already in dictionary: increase
-                 counts[number] = counts[number] + 1
-           
-        num_lines += 1
-        if num_lines % 500000 == 0: # progress bar
-            print("...{0} lines".format(num_lines))
+__version__ = '0.1'
 
-print("processed {0} lines".format(num_lines))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Numerals extractor')
+    parser.add_argument("--min", default = 1, type = int,
+                        help = 'minimal numeral to count')
+    parser.add_argument("--max", default = 100, type = int,
+                        help = 'maximal numeral to count')
+    parser.add_argument("--language", default = 'en',
+                        help = 'the corpus language (en, de, ...)')
+    parser.add_argument('-p', '--plot', action="store_true",
+                        help = 'provide a bar plot of the results')
+    parser.add_argument("--version", action='version',
+                        version='%(prog)s version ' + __version__,
+                        help = 'output version information and exit')
+    parser.add_argument("file", nargs='*',
+                        help = 'the file(s) to process')
+    args = parser.parse_args()
 
-# plot a bar chart
-numbers = range(n_low, n_high + 1)
-values = list(map(lambda x: counts[x] if x in counts else 0, numbers))
-plt.bar(numbers, values)
-plt.show()
+    # initialize the language object (language of the corpus
+    # determines number separators (1,000 vs. 1.000), assume English
+    # if not given
+    try:
+        language = Language.create(args.language)
+    except LookupError:
+        print("error: language \"{}\" is not supported.".
+              format(args.language), file=sys.stderr)
+        sys.exit(1)
+
+    # now do the processing ...
+    processor = Processor(language)
+    processor.setLimits(args.min, args.max)
+    processor.verbosity = 2
+    if not args.file:
+        processor.processFile(sys.stdin)
+    for name in args.file:
+        if name == '-':
+            processor.processFile(sys.stdin)
+            continue
+
+        # try to find determine the path to the name ...
+        if not os.path.exists(name):
+            if ('WORTSCHATZ_ROOT' in os.environ and
+                os.path.exists(os.path.join(os.environ['WORTSCHATZ_ROOT'],
+                                            name))):
+                name = os.path.join(os.environ['WORTSCHATZ_ROOT'], name)
+            else:
+                print("error: no file called \"{}\"".
+                      format(name),file=sys.stderr)
+                sys.exit(1)
+
+        if os.path.isdir(name):
+            guess = os.path.join(name, os.path.basename(name)+"-sentences.txt")
+            if os.path.isfile(guess):
+                name = guess
+            else:
+                print("error: directory \"{}\" does not contain a valid file".
+                      format(name),file=sys.stderr)
+                sys.exit(1)
+
+        # ... and run the processor
+        print("Using \"{}\"".format(name), file=sys.stderr)
+        with open(name, 'r') as inputStream:
+            processor.processFile(inputStream)
+
+    # finally plot the results
+    if args.plot:
+        processor.plotBars()
